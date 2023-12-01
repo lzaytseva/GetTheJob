a250c057ed6bf4606119af5a5e0fba898c431b34
Max <bespalov.m.9@ya.ru>
2023-12-01 21:45:26 +0300

    refactoring in root activity class

--- app/src/main/java/ru/practicum/android/diploma/core/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/ui/RootActivity.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
 
     private val binding by lazy {
-        //ActivityRootBinding.inflate(layoutInflater)
+        ActivityRootBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
+
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
