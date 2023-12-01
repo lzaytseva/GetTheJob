fdeebb83e826baaeb87bef7fd0be2cb9f026d627
Dmitry Kryushnikov <civ02@mail.ru>
2023-12-02 04:33:33 +0500

    убран lateinit binding в RootActivity

--- app/src/main/java/ru/practicum/android/diploma/core/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/ui/RootActivity.kt
@@ -9,11 +9,10 @@ import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRootBinding
+    private val binding: ActivityRootBinding by lazy { ActivityRootBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navHostFragment =
