441b0bf66b0aeae35d035351dfbdd3ecb9734be4
Dmitry Kryushnikov <civ02@mail.ru>
2023-12-02 01:56:06 +0500

    binding, NavHostFragment, NavController в RootActivity

--- app/src/main/java/ru/practicum/android/diploma/core/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/ui/RootActivity.kt
@@ -2,13 +2,23 @@ package ru.practicum.android.diploma.core.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.root_fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
