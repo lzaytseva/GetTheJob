fc4889cf9f03bc5989f06a77ae9b9559d80d842d
ledrabbit <lvzaytseva1@gmail.com>
2023-12-04 02:36:28 +0300

    VacancyDto переименован в VacancyEntity и перемещен в пакет entity

--- app/src/main/java/ru/practicum/android/diploma/core/data/room/entity/VacancyEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/data/room/entity/VacancyEntity.kt
@@ -1,15 +1,15 @@
-package ru.practicum.android.diploma.core.data.room
+package ru.practicum.android.diploma.core.data.room.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "favorites_table")
-data class VacancyDto(
+data class VacancyEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
     val vacancyId: String,
     val name: String,
-//    val area: AreaDto,
+//    val area: Area,
 //    val salary: Salary?,
 //    val experience: Experience?,
 //    val schedule: Schedule,
