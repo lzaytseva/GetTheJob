66880d8be12fa5234680466be27fa6cfdac2114c
ledrabbit <lvzaytseva1@gmail.com>
2023-12-04 02:13:17 +0300

    добавлены провайд методы для сервиса и ретрофита

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -7,7 +7,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
 import ru.practicum.android.diploma.core.data.navigation.ExternalNavigatorImpl
+import ru.practicum.android.diploma.core.data.network.HhApiService
+import ru.practicum.android.diploma.core.data.network.NetworkClient
+import ru.practicum.android.diploma.core.data.network.RetrofitNetworkClient
 import ru.practicum.android.diploma.core.data.room.GtjDatabase
 import ru.practicum.android.diploma.core.domain.api.ExternalNavigator
 
@@ -24,4 +28,20 @@ class DataModule {
         return ExternalNavigatorImpl(appContext)
     }
 
+    @Provides
+    fun provideHhService(): HhApiService {
+        return Retrofit.Builder()
+            .baseUrl(HH_BASE_URL)
+            .build()
+            .create(HhApiService::class.java)
+    }
+
+    @Provides
+    fun provideRetrofitNetworkClient(@ApplicationContext context: Context, hhService: HhApiService): NetworkClient {
+        return RetrofitNetworkClient(context, hhService)
+    }
+
+    companion object {
+        private const val HH_BASE_URL = "https://api.hh.ru/"
+    }
 }
