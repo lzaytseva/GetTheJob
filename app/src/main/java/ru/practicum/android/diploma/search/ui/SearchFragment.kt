aded5cd922e37af2380a2243e9ebf4e6a76b9fcd
Eugene Pimenov <109866375+Megrief@users.noreply.github.com>
2023-12-05 16:18:25 +0500

    Update SearchFragment.kt

--- app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
@@ -8,14 +8,14 @@ import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.core.domain.models.ErrorType
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 import ru.practicum.android.diploma.search.domain.model.VacancyInList
 import ru.practicum.android.diploma.search.presentation.SearchScreenState
 import ru.practicum.android.diploma.search.presentation.SearchViewModel
 import ru.practicum.android.diploma.util.BindingFragment
-import dagger.hilt.android.AndroidEntryPoint
-import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.core.domain.models.ErrorType
 
 class SearchFragment : BindingFragment<FragmentSearchBinding>() {
 
