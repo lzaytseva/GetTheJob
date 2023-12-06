fc2e00183cb1587db57803c5add2823c6f6c07e2
megrief <eu.pim@mail.ru>
2023-12-06 10:34:00 +0500

    Добавлены классы adapter и viewHolder Изменена структура entity класса VacancyInList Имя ресурса placeholder приведено в соответсвие с codeStyle

--- app/src/main/java/ru/practicum/android/diploma/search/domain/model/VacancyInList.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/model/VacancyInList.kt
@@ -1,11 +1,9 @@
 package ru.practicum.android.diploma.search.domain.model
 
-import ru.practicum.android.diploma.core.data.dto.innerdto.EmployerDto
-import ru.practicum.android.diploma.core.data.dto.innerdto.SalaryDto
-
 data class VacancyInList(
     val id: String,
     val name: String,
-    val salaryDto: SalaryDto?,
-    val employerDto: EmployerDto
+    val salary: String,
+    val employerName: String,
+    val logo: String?
 )
