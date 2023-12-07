6c8d4e8e76af104a1bab0d90e334a6f211a3a362
Max <bespalov.m.9@ya.ru>
2023-12-07 20:15:51 +0300

    VacancyDetailsRepository and VacancyDetailsRepositoryImpl were added

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,9 +1,21 @@
 package ru.practicum.android.diploma.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import ru.practicum.android.diploma.core.data.network.NetworkClient
+import ru.practicum.android.diploma.vacancydetails.data.VacancyDetailsRepositoryImpl
+import ru.practicum.android.diploma.vacancydetails.domain.api.VacancyDetailsRepository
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class RepositoryModule
+class RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideVacancyDetailsRepository(networkClient: NetworkClient): VacancyDetailsRepository {
+        return VacancyDetailsRepositoryImpl(networkClient)
+    }
+}
