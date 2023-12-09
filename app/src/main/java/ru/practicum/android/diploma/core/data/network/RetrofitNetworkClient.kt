e280eba0e59e81f9ca840e41e11aa60db9bca4b2
ledrabbit <lvzaytseva1@gmail.com>
2023-12-09 18:57:54 +0300

    добавлена обработка IndustriesRequest в метод doRequest

--- app/src/main/java/ru/practicum/android/diploma/core/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/data/network/RetrofitNetworkClient.kt
@@ -3,7 +3,9 @@ package ru.practicum.android.diploma.core.data.network
 import android.content.Context
 import android.util.Log
 import retrofit2.HttpException
+import ru.practicum.android.diploma.filters.data.dto.IndustriesRequest
 import ru.practicum.android.diploma.core.data.dto.requests.VacancyDetailsSearchRequest
+import ru.practicum.android.diploma.filters.data.dto.IndustriesResponse
 import ru.practicum.android.diploma.core.data.dto.responses.Response
 import ru.practicum.android.diploma.core.data.dto.responses.VacancyDetailsSearchResponse
 import ru.practicum.android.diploma.util.ConnectionChecker
@@ -22,6 +24,7 @@ class RetrofitNetworkClient(
 
         return when (request) {
             is VacancyDetailsSearchRequest -> getVacancyDetailsById(request.id)
+            is IndustriesRequest -> getIndustries()
 
             else -> Response().apply { resultCode = RC_NOK_SERVER_ERROR }
 
@@ -34,6 +37,22 @@ class RetrofitNetworkClient(
             if (response.code() == RC_OK && response.body() != null) {
                 VacancyDetailsSearchResponse(response.body()!!)
                     .apply { resultCode = RC_OK }
+            } else {
+                Response().apply { resultCode = RC_NOK }
+            }
+        } catch (e: HttpException) {
+            Log.e(TAG, e.toString())
+            Response().apply { resultCode = RC_NOK }
+        }
+    }
+
+    private suspend fun getIndustries(): Response {
+        return try {
+            val response = hhService.getIndustries()
+            if (response.body() != null) {
+                IndustriesResponse(response.body()!!).apply {
+                    resultCode = RC_OK
+                }
             } else {
                 Response().apply { resultCode = RC_NOK }
             }
