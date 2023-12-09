bdd8f9adaeefa4fa979a6b32b331bd764f589f6c
Dmitry Kryushnikov <civ02@mail.ru>
2023-12-09 22:28:12 +0500

    di для репозитория удаления вакансии из бд

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -5,6 +5,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import ru.practicum.android.diploma.core.data.network.NetworkClient
+import ru.practicum.android.diploma.core.data.room.AppDatabase
+import ru.practicum.android.diploma.favorites.data.DeleteVacancyRepositoryImpl
+import ru.practicum.android.diploma.favorites.domain.api.DeleteVacancyRepository
 import ru.practicum.android.diploma.vacancydetails.data.VacancyDetailsRepositoryImpl
 import ru.practicum.android.diploma.vacancydetails.domain.api.VacancyDetailsRepository
 import javax.inject.Singleton
@@ -17,5 +20,11 @@ class RepositoryModule {
     @Singleton
     fun provideVacancyDetailsRepository(networkClient: NetworkClient): VacancyDetailsRepository {
         return VacancyDetailsRepositoryImpl(networkClient)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDeleteVacancyRepository(addDatabase: AppDatabase): DeleteVacancyRepository {
+        return DeleteVacancyRepositoryImpl(addDatabase)
     }
 }
