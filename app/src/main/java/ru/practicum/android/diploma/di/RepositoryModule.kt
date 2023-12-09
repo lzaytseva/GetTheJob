3c587163ee2c6272be9dde9a225c4ffba5ca6001
Dmitry Kryushnikov <civ02@mail.ru>
2023-12-09 23:25:39 +0500

    di репозитория получения детальной информации о вакансии

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -5,6 +5,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import ru.practicum.android.diploma.core.data.network.NetworkClient
+import ru.practicum.android.diploma.core.data.room.AppDatabase
 import ru.practicum.android.diploma.core.domain.api.GetDataRepo
 import ru.practicum.android.diploma.filters.data.repository.IndustriesRepositoryImpl
 import ru.practicum.android.diploma.filters.domain.model.Industry
@@ -27,5 +28,11 @@ class RepositoryModule {
     @Singleton
     fun provideIndustriesRepository(networkClient: NetworkClient): GetDataRepo<Resource<List<Industry>>> {
         return IndustriesRepositoryImpl(networkClient)
+    }
+
+    @Provides
+    @Singleton
+    fun provideVacancyDetailsRepository(appDatabase: AppDatabase): VacancyDetailsRepository {
+        return VacancyDetailsRepositoryImpl(appDatabase)
     }
 }
