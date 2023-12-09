94e897061ebb7f948f42378686cc74d635a91a43
ledrabbit <lvzaytseva1@gmail.com>
2023-12-09 20:41:27 +0300

    форматирование

--- app/src/main/java/ru/practicum/android/diploma/filters/domain/model/IndustryScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/domain/model/IndustryScreenState.kt
@@ -1,10 +1,10 @@
 package ru.practicum.android.diploma.filters.domain.model
 
 sealed interface IndustryScreenState {
-    data object Loading: IndustryScreenState
-    data class Content(val industries: List<Industry>): IndustryScreenState
+    data object Loading : IndustryScreenState
+    data class Content(val industries: List<Industry>) : IndustryScreenState
 
-    data class Error(val message: String): IndustryScreenState
+    data class Error(val message: String) : IndustryScreenState
 
-    data object Empty: IndustryScreenState
+    data object Empty : IndustryScreenState
 }
