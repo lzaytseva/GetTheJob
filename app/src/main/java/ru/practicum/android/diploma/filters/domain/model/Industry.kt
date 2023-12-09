1a9506fa59d580d26440d0a4ff29a88dab48cc2d
ledrabbit <lvzaytseva1@gmail.com>
2023-12-09 22:01:00 +0300

    поле selected изменено с val на var

--- app/src/main/java/ru/practicum/android/diploma/filters/domain/model/Industry.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/domain/model/Industry.kt
@@ -3,5 +3,5 @@ package ru.practicum.android.diploma.filters.domain.model
 data class Industry(
     val id: String,
     val name: String,
-    val selected: Boolean = false
+    var selected: Boolean = false
 )
