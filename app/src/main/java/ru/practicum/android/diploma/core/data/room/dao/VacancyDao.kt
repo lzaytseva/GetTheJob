486a3d1f67e3cec29ea0e74d128b7e8ac0499de8
Dmitry Kryushnikov <civ02@mail.ru>
2023-12-09 16:07:04 +0500

    методы в VacancyDao для сохранения вакансии, удаления, получения всего списка, получения вакансии по id

--- app/src/main/java/ru/practicum/android/diploma/core/data/room/dao/VacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/data/room/dao/VacancyDao.kt
@@ -1,9 +1,24 @@
 package ru.practicum.android.diploma.core.data.room.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.practicum.android.diploma.core.data.room.entity.VacancyEntity
+import ru.practicum.android.diploma.core.data.room.entity.VacancyShort
 
 @Dao
 interface VacancyDao {
-//    @Insert(entity = VacancyDto::class, onConflict = OnConflictStrategy.REPLACE)
-//    fun save(entity: VacancyDto)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveVacancy(entity: VacancyEntity)
+
+    @Delete
+    suspend fun deleteVacancy(entity: VacancyEntity)
+
+    @Query("SELECT vacancyId, name, salaryCurrency, salaryFrom, salaryTo, employerName, logoUrl FROM favorites_table ORDER BY id DESC")
+    suspend fun getVacancyList(): List<VacancyShort>
+
+    @Query("SELECT * FROM favorites_table WHERE vacancyId = :vacancyId LIMIT 1")
+    suspend fun getVacancyById(vacancyId: String): VacancyEntity
 }
