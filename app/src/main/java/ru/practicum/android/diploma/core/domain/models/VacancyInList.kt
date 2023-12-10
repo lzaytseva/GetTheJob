95494baedf416601ee0dfc13834444b55ce2e702
Dmitry Kryushnikov <civ02@mail.ru>
2023-12-10 22:56:23 +0500

    новые свойства VacancyInList

--- app/src/main/java/ru/practicum/android/diploma/core/domain/models/VacancyInList.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/domain/models/VacancyInList.kt
@@ -3,7 +3,9 @@ package ru.practicum.android.diploma.core.domain.models
 data class VacancyInList(
     val id: String,
     val name: String,
-    val salary: String,
+    val salaryCurrency: String?,
+    val salaryFrom: Int?,
+    val salaryTo: Int?,
     val employerName: String,
     val logo: String?
 )
