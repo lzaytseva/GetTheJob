895aaabfd7212f58b208b33c8c9e8607db5eb581
Eugene Pimenov <109866375+Megrief@users.noreply.github.com>
2023-12-11 16:47:11 +0500

    Update VacanciesSearchRequest.kt

--- app/src/main/java/ru/practicum/android/diploma/core/data/dto/requests/VacanciesSearchRequest.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/data/dto/requests/VacanciesSearchRequest.kt
@@ -1,11 +1,11 @@
 package ru.practicum.android.diploma.core.data.dto.requests
 
- private const val TEXT = "text"
- private const val SALARY = "salary"
- private const val AREA = "area"
- private const val INDUSTRY = "industry"
- private const val ONLY_WITH_SALARY = "only_with_salary"
- private const val CURRENCY = "currency"
+private const val TEXT = "text"
+private const val SALARY = "salary"
+private const val AREA = "area"
+private const val INDUSTRY = "industry"
+private const val ONLY_WITH_SALARY = "only_with_salary"
+private const val CURRENCY = "currency"
 
 data class VacanciesSearchRequest(
     val text: String,
