c7930823d51eecfc239f2914a59493d1fe038544
Eugene Pimenov <109866375+Megrief@users.noreply.github.com>
2023-12-11 17:00:59 +0500

    Update VacancySearchMapper.kt

--- app/src/main/java/ru/practicum/android/diploma/search/util/VacancySearchMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/util/VacancySearchMapper.kt
@@ -1,7 +1,15 @@
 package ru.practicum.android.diploma.search.util
 
 import ru.practicum.android.diploma.core.data.dto.VacancySearchDto
+import ru.practicum.android.diploma.core.data.dto.requests.VacanciesSearchRequest
 import ru.practicum.android.diploma.search.domain.model.VacancyInList
+
+private const val TEXT = "text"
+private const val SALARY = "salary"
+private const val AREA = "area"
+private const val INDUSTRY = "industry"
+private const val ONLY_WITH_SALARY = "only_with_salary"
+private const val CURRENCY = "currency"
 
 fun VacancySearchDto.toVacancyInList(): VacancyInList =
     VacancyInList(
@@ -14,3 +22,12 @@ fun VacancySearchDto.toVacancyInList(): VacancyInList =
         employerName = employerDto.name,
         logo = employerDto.logoUrlsDto?.art90
     )
+    
+fun VacancySearchRequest.toQueryMap(): Map<String, String> = buildMap {
+    put(TEXT, text)
+    salary?.also { put(SALARY, it) }
+    regionId?.also { put(AREA, it) }
+    industryId?.also { put(INDUSTRY, it) }
+    salaryFlag?.also { put(ONLY_WITH_SALARY, it.toString()) }
+    currency?.also { put(CURRENCY, it) }
+}
