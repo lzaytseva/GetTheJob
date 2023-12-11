26f968935c5fc28e5c24725c159c7883b6c0fea3
Eugene Pimenov <109866375+Megrief@users.noreply.github.com>
2023-12-11 16:56:59 +0500

    Update VacanciesSearchRequest.kt

--- app/src/main/java/ru/practicum/android/diploma/core/data/dto/requests/VacanciesSearchRequest.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/data/dto/requests/VacanciesSearchRequest.kt
@@ -1,12 +1,5 @@
 package ru.practicum.android.diploma.core.data.dto.requests
 
-private const val TEXT = "text"
-private const val SALARY = "salary"
-private const val AREA = "area"
-private const val INDUSTRY = "industry"
-private const val ONLY_WITH_SALARY = "only_with_salary"
-private const val CURRENCY = "currency"
-
 data class VacanciesSearchRequest(
     val text: String,
     val salary: String? = null,
@@ -14,13 +7,4 @@ data class VacanciesSearchRequest(
     val industryId: String? = null,
     val salaryFlag: Boolean? = null,
     val currency: String? = null
-) {
-    fun toQueryMap(): Map<String, String> = buildMap {
-        put(TEXT, text)
-        salary?.also { put(SALARY, it) }
-        regionId?.also { put(AREA, it) }
-        industryId?.also { put(INDUSTRY, it) }
-        salaryFlag?.also { put(ONLY_WITH_SALARY, it.toString()) }
-        currency?.also { put(CURRENCY, it) }
-    }
-}
+)
