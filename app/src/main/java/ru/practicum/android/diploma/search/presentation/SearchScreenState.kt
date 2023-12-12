bd4209cd166f711bc3846cffa86b33949f0b5279
megrief <eu.pim@mail.ru>
2023-12-12 11:04:24 +0500

    Исправлено сохранение состояния строки поиска: - Удалено поле state из SearchScreenState - Добавлен SingleLiveEvent _savedQueryState - Сохранение состояния производится только при вызове onPaused восстановление в onViewCreated
    SingleLiveEvent<ErrorType> заменен на data class в SearchScreenState
    
    hideContent:
    - переписан, для большей гибкости использования
    - удалены ненужные вызовы метода

--- app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchScreenState.kt
@@ -4,10 +4,10 @@ import ru.practicum.android.diploma.core.domain.models.ErrorType
 import ru.practicum.android.diploma.search.domain.model.VacancyInList
 
 sealed class SearchScreenState {
-    var state: String = ""
 
     data object Loading : SearchScreenState()
     data class Error(val error: ErrorType) : SearchScreenState()
     data class Content(val content: List<VacancyInList>) : SearchScreenState()
     data object LoadingNextPage : SearchScreenState()
+    data class LoadingNextPageError(val error: ErrorType) : SearchScreenState()
 }
