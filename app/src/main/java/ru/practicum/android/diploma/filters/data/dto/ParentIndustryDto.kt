24011eff8aa64e4f18fd60fd1642c6b03175646e
ledrabbit <lvzaytseva1@gmail.com>
2023-12-15 15:09:10 +0300

    рефакторинг

--- app/src/main/java/ru/practicum/android/diploma/filters/data/dto/ParentIndustryDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/data/dto/ParentIndustryDto.kt
@@ -3,7 +3,10 @@ package ru.practicum.android.diploma.filters.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class ParentIndustryDto(
-    @SerializedName("id") val id: String,
-    @SerializedName("industries") val subIndustries: List<IndustryDto>,
-    @SerializedName("name") val name: String
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("industries")
+    val subIndustries: List<IndustryDto>,
+    @SerializedName("name")
+    val name: String
 )
