db3bc79688f132756d13cca7929caf9491a6084c
Dmitry Kryushnikov <civ02@mail.ru>
2023-12-16 16:28:26 +0500

    класс AreaDto и HhApiService

---
+++ app/src/main/java/ru/practicum/android/diploma/filters/data/dto/AreaDto.kt
@@ -0,0 +1,11 @@
+package ru.practicum.android.diploma.filters.data.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class AreaDto(
+    val areas: List<AreaDto>,
+    val id: String,
+    val name: String,
+    @SerializedName("parent_id")
+    val parentId: String?
+)
