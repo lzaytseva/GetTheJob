30c27b2a1466f03f2b09e33a35b714a06d075b6d
Dmitry Kryushnikov <civ02@mail.ru>
2023-12-17 00:58:11 +0500

    методы в RetrofitNetworkClient

--- app/src/main/java/ru/practicum/android/diploma/core/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/data/network/RetrofitNetworkClient.kt
@@ -10,8 +10,12 @@ import ru.practicum.android.diploma.core.data.dto.requests.VacanciesSearchReques
 import ru.practicum.android.diploma.core.data.dto.requests.VacancyDetailsSearchRequest
 import ru.practicum.android.diploma.core.data.dto.responses.Response
 import ru.practicum.android.diploma.core.data.dto.responses.VacancyDetailsSearchResponse
+import ru.practicum.android.diploma.filters.data.dto.AllAreasRequest
+import ru.practicum.android.diploma.filters.data.dto.AllAreasResponse
 import ru.practicum.android.diploma.filters.data.dto.CountriesRequest
 import ru.practicum.android.diploma.filters.data.dto.CountriesResponse
+import ru.practicum.android.diploma.filters.data.dto.CountryByIdRequest
+import ru.practicum.android.diploma.filters.data.dto.CountryByIdResponse
 import ru.practicum.android.diploma.filters.data.dto.IndustriesRequest
 import ru.practicum.android.diploma.filters.data.dto.IndustriesResponse
 import ru.practicum.android.diploma.search.util.toQueryMap
@@ -34,6 +38,8 @@ class RetrofitNetworkClient(
                 is VacanciesSearchRequest -> getVacanciesList(request.toQueryMap())
                 is SimilarVacanciesSearchRequest -> getSimilarVacanciesById(request.id)
                 is CountriesRequest -> getCountries()
+                is AllAreasRequest -> getAllAreas()
+                is CountryByIdRequest -> getCountryById(request.id)
                 else -> Response().apply { resultCode = CODE_WRONG_REQUEST }
             }
         }
@@ -100,6 +106,34 @@ class RetrofitNetworkClient(
             val response = hhService.getCountries()
             if (response.body() != null) {
                 CountriesResponse(response.body()!!).apply { resultCode = CODE_SUCCESS }
+            } else {
+                Response().apply { resultCode = CODE_SERVER_ERROR }
+            }
+        } catch (e: HttpException) {
+            Log.e(TAG, e.toString())
+            Response().apply { resultCode = CODE_SERVER_ERROR }
+        }
+    }
+
+    private suspend fun getAllAreas(): Response {
+        return try {
+            val response = hhService.getAllAreas()
+            if (response.body() != null) {
+                AllAreasResponse(response.body()!!).apply { resultCode = CODE_SUCCESS }
+            } else {
+                Response().apply { resultCode = CODE_SERVER_ERROR }
+            }
+        } catch (e: HttpException) {
+            Log.e(TAG, e.toString())
+            Response().apply { resultCode = CODE_SERVER_ERROR }
+        }
+    }
+
+    private suspend fun getCountryById(id: String): Response {
+        return try {
+            val response = hhService.getAreaById(id)
+            if (response.body() != null) {
+                CountryByIdResponse(response.body()!!).apply { resultCode = CODE_SUCCESS }
             } else {
                 Response().apply { resultCode = CODE_SERVER_ERROR }
             }
