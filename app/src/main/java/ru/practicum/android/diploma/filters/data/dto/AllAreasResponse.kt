33b4e1437b3053d71a494c477de7e82e82008df9
Dmitry Kryushnikov <civ02@mail.ru>
2023-12-17 00:45:41 +0500

    ответы из сети

---
+++ app/src/main/java/ru/practicum/android/diploma/filters/data/dto/AllAreasResponse.kt
@@ -0,0 +1,5 @@
+package ru.practicum.android.diploma.filters.data.dto
+
+import ru.practicum.android.diploma.core.data.dto.responses.Response
+
+data class AllAreasResponse(val areas: List<AreaDto>): Response()
