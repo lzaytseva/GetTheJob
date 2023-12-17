5c09dc4e67d096d5890d9b6a8d0ea6172cefd4b9
ledrabbit <lvzaytseva1@gmail.com>
2023-12-17 20:18:03 +0300

    Добавлены поля для названия отрасли, страны и региона

--- app/src/main/java/ru/practicum/android/diploma/core/domain/models/Filters.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/domain/models/Filters.kt
@@ -2,9 +2,12 @@ package ru.practicum.android.diploma.core.domain.models
 
 data class Filters(
     val regionId: String?,
+    val regionName: String?,
     val countryId: String?,
+    val countryName: String?,
     val salary: String?,
     val salaryFlag: Boolean?,
     val industryId: String?,
+    val industryName: String,
     val currency: String?
 )
