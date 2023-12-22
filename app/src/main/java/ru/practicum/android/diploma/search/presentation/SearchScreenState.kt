b4c31a3309874350a71773d3ffad65f69722e285
Dmitry Kryushnikov <civ02@mail.ru>
2023-12-23 00:32:56 +0500

    Revert "new screen state was added for search screen: NotStarted"
    This reverts commit 92720752

--- app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchScreenState.kt
@@ -5,7 +5,6 @@ import ru.practicum.android.diploma.search.domain.model.Vacancy
 
 sealed class SearchScreenState {
 
-    data object NotStarted : SearchScreenState()
     data object Loading : SearchScreenState()
     data object Empty : SearchScreenState()
     data class Error(val error: ErrorType) : SearchScreenState()
